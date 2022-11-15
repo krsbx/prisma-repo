c0f3f2f50b07cf7d8f7e7c8a12d33ff9cbeb41c4
KRSBX <firdaus.sati51@gmail.com>
2022-11-15 16:11:30 +0700

    chore: add basic types for models in repo template

--- template/repository.ts
+++ template/repository.ts
@@ -1,6 +1,22 @@
 import _ from 'lodash';
 import BaseRepository from './baseRepository';
-import { AnyRecord, ModelStructure, MODELS_NAME } from './prisma-repo';
+import { AnyRecord, ModelStructure, ModelTypes, ModelScalarFields, MODELS_NAME  } from './prisma-repo';
+
+// This type will be used if you want to extends the functions in {{ repository }} Class
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+type Where = ModelTypes[typeof {{ repositoryName }}]['Where'];
+type Select = ModelTypes[typeof {{ repositoryName }}]['Select'];
+type Include = ModelTypes[typeof {{ repositoryName }}]['Include'];
+type Create = ModelTypes[typeof {{ repositoryName }}]['Create'];
+type Update = ModelTypes[typeof {{ repositoryName }}]['Update'];
+type Cursor = ModelTypes[typeof {{ repositoryName }}]['Cursor'];
+type Order = ModelTypes[typeof {{ repositoryName }}]['Order'];
+type Delegate = ModelTypes[typeof {{ repositoryName }}]['Delegate'];
+type Scalar = ModelScalarFields<typeof {{ repositoryName }}>;
+type Model = ModelStructure[typeof {{ repositoryName }}];
+/*  eslint-enable @typescript-eslint/no-unused-vars */
+
 
 class {{ repository }} extends BaseRepository({{ repositoryName }}) {
 }
